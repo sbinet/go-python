737607ad53e2ca56f66438babec21f73fb8abb20
Sebastien Binet <binet@farnsworth>
2011-01-03 17:55:45 +0100

    put all numeric types into numeric.go and sequence types into sequence.go

--- pkg/numeric.go
+++ pkg/numeric.go
@@ -11,6 +11,11 @@ package python
 //int _gopy_PyBool_Check(PyObject *o) { return PyBool_Check(o); }
 //PyObject *_gopy_pyfalse() { return Py_False; }
 //PyObject *_gopy_pytrue() { return Py_True; }
+//int _gopy_PyFloat_Check(PyObject *o) { return PyFloat_Check(o); }
+//int _gopy_PyFloat_CheckExact(PyObject *o) { return PyFloat_CheckExact(o); }
+//double _gopy_PyFloat_AS_DOUBLE(PyObject *pyfloat) { return PyFloat_AS_DOUBLE(pyfloat); }
+//int _gopy_PyComplex_Check(PyObject *o) { return PyComplex_Check(o); }
+//int _gopy_PyComplex_CheckExact(PyObject *o) { return PyComplex_CheckExact(o); }
 import "C"
 import "unsafe"
 
@@ -432,6 +437,196 @@ New in version 2.3.
 */
 func PyBool_FromLong(v int) *PyObject {
 	return togo(C.PyBool_FromLong(C.long(v)))
+}
+
+////////// float ////////////
+/*
+int PyFloat_Check(PyObject *p)
+Return true if its argument is a PyFloatObject or a subtype of PyFloatObject.
+
+Changed in version 2.2: Allowed subtypes to be accepted.
+*/
+func PyFloat_Check(self *PyObject) bool {
+	return int2bool(C._gopy_PyFloat_Check(topy(self)))
+}
+
+/*
+int PyFloat_CheckExact(PyObject *p)
+Return true if its argument is a PyFloatObject, but not a subtype of PyFloatObject.
+
+New in version 2.2.
+*/
+func PyFloat_CheckExact(self *PyObject) bool {
+	return int2bool(C._gopy_PyFloat_CheckExact(topy(self)))
+}
+
+/*
+PyObject* PyFloat_FromString(PyObject *str, char **pend)
+Return value: New reference.
+Create a PyFloatObject object based on the string value in str, or NULL on failure. The pend argument is ignored. It remains only for backward compatibility.
+*/
+func PyFloat_FromString(str *PyObject) *PyObject {
+	return togo(C.PyFloat_FromString(topy(str), nil))
+}
+
+/*
+PyObject* PyFloat_FromDouble(double v)
+Return value: New reference.
+Create a PyFloatObject object from v, or NULL on failure.
+*/
+func PyFloat_FromDouble(v float) *PyObject {
+	return togo(C.PyFloat_FromDouble(C.double(v)))
+}
+
+/*
+double PyFloat_AsDouble(PyObject *pyfloat)
+Return a C double representation of the contents of pyfloat. If pyfloat is not a Python floating point object but has a __float__() method, this method will first be called to convert pyfloat into a float.
+*/
+func PyFloat_AsDouble(self *PyObject) float {
+	return float(C.PyFloat_AsDouble(topy(self)))
+}
+
+/*
+double PyFloat_AS_DOUBLE(PyObject *pyfloat)
+Return a C double representation of the contents of pyfloat, but without error checking.
+*/
+func PyFloat_AS_DOUBLE(self *PyObject) float {
+	return float(C._gopy_PyFloat_AS_DOUBLE(topy(self)))
+}
+
+/*
+PyObject* PyFloat_GetInfo(void)
+Return a structseq instance which contains information about the precision, minimum and maximum values of a float. It’s a thin wrapper around the header file float.h.
+
+New in version 2.6.
+*/
+func PyFloat_GetInfo() *PyObject {
+	return togo(C.PyFloat_GetInfo())
+}
+
+/*
+double PyFloat_GetMax()
+Return the maximum representable finite float DBL_MAX as C double.
+
+New in version 2.6.
+*/
+func PyFloat_GetMax() float {
+	return float(C.PyFloat_GetMax())
+}
+
+/*
+double PyFloat_GetMin()
+Return the minimum normalized positive float DBL_MIN as C double.
+
+New in version 2.6.
+*/
+func PyFloat_GetMin() float {
+	return float(C.PyFloat_GetMin())
+}
+
+/*
+int PyFloat_ClearFreeList()
+Clear the float free list. Return the number of items that could not be freed.
+
+New in version 2.6.
+*/
+func PyFloat_ClearFreeList() int {
+	return int(C.PyFloat_ClearFreeList())
+}
+
+/*
+void PyFloat_AsString(char *buf, PyFloatObject *v)
+Convert the argument v to a string, using the same rules as str(). The length of buf should be at least 100.
+
+This function is unsafe to call because it writes to a buffer whose length it does not know.
+
+Deprecated since version 2.7: Use PyObject_Str() or PyOS_double_to_string() instead.
+*/
+func PyFloat_AsString(buf []byte, v *C.PyFloatObject) {
+	//FIXME ?
+	panic("not implemented")
+}
+
+/*
+void PyFloat_AsReprString(char *buf, PyFloatObject *v)
+Same as PyFloat_AsString, except uses the same rules as repr(). The length of buf should be at least 100.
+
+This function is unsafe to call because it writes to a buffer whose length it does not know.
+
+Deprecated since version 2.7: Use PyObject_Repr() or PyOS_double_to_string() instead.
+*/
+func PyFloat_AsReprString(buf []byte, v *C.PyFloatObject) {
+	//FIXME ?
+	panic("not implemented")
+}
+
+
+/////////// complex ///////////
+
+/*
+int PyComplex_Check(PyObject *p)
+Return true if its argument is a PyComplexObject or a subtype of PyComplexObject.
+
+Changed in version 2.2: Allowed subtypes to be accepted.
+*/
+func PyComplex_Check(self *PyObject) bool {
+	return int2bool(C._gopy_PyComplex_Check(topy(self)))
+}
+
+/*
+int PyComplex_CheckExact(PyObject *p)
+Return true if its argument is a PyComplexObject, but not a subtype of PyComplexObject.
+
+New in version 2.2.
+*/
+func PyComplex_CheckExact(self *PyObject) bool {
+	return int2bool(C._gopy_PyComplex_CheckExact(topy(self)))
+}
+
+/*
+PyObject* PyComplex_FromCComplex(Py_complex v)
+Return value: New reference.
+Create a new Python complex number object from a C Py_complex value.
+*/
+func PyComplex_FromCComplex(v C.Py_complex) *PyObject {
+	//FIXME ? use go-complex ?
+	return togo(C.PyComplex_FromCComplex(v))
+}
+
+/*
+PyObject* PyComplex_FromDoubles(double real, double imag)
+Return value: New reference.
+Return a new PyComplexObject object from real and imag.
+*/
+func PyComplex_FromDoubles(real, imag float64) *PyObject {
+	return togo(C.PyComplex_FromDoubles(C.double(real), C.double(imag)))
+}
+
+/*
+double PyComplex_RealAsDouble(PyObject *op)
+Return the real part of op as a C double.
+*/
+func PyComplex_RealAsDouble(op *PyObject) float64 {
+	return float64(C.PyComplex_RealAsDouble(topy(op)))
+}
+
+/*
+double PyComplex_ImagAsDouble(PyObject *op)
+Return the imaginary part of op as a C double.
+*/
+func PyComplex_ImagAsDouble(op *PyObject) float64 {
+	return float64(C.PyComplex_ImagAsDouble(topy(op)))
+}
+
+/*
+Py_complex PyComplex_AsCComplex(PyObject *op)
+Return the Py_complex value of the complex number op.
+
+Changed in version 2.6: If op is not a Python complex number object but has a __complex__() method, this method will first be called to convert op to a Python complex number object.
+*/
+func PyComplex_AsCComplex(op *PyObject) C.Py_complex {
+	// FIXME ? use go-complex ?
+	return C.PyComplex_AsCComplex(topy(op))
 }
 
 
--- pkg/sequence.go
+++ pkg/sequence.go
@@ -16,6 +16,7 @@ package python
 //int _gopy_PyString_Check(PyObject *o) { return PyString_Check(o); }
 //Py_ssize_t _gopy_PyString_GET_SIZE(PyObject *o) { return PyString_GET_SIZE(o);}
 //char* _gopy_PyString_AS_STRING(PyObject *o) { return PyString_AS_STRING(o); }
+//int _gopy_PyObject_CheckBuffer(PyObject *obj) { return PyObject_CheckBuffer(obj); }
 import "C"
 import "unsafe"
 import "os"
@@ -615,5 +616,133 @@ func PyString_AsEncodedObject(self *PyObject, encoding, errors string) *PyObject
 
 	return togo(C.PyString_AsEncodedObject(topy(self), c_encoding, c_errors))
 }
+
+/////////// buffer ///////////
+// Py_buffer layer
+type Py_buffer struct {
+	ptr *C.Py_buffer
+}
+
+type PyBUF_Flag int
+
+const (
+	PyBUF_SIMPLE = PyBUF_Flag(C.PyBUF_SIMPLE)
+	PyBUF_WRITABLE = PyBUF_Flag(C.PyBUF_WRITABLE)
+	PyBUF_STRIDES = PyBUF_Flag(C.PyBUF_STRIDES)
+	PyBUF_ND = PyBUF_Flag(C.PyBUF_ND)
+	PyBUF_C_CONTIGUOUS = PyBUF_Flag(C.PyBUF_C_CONTIGUOUS)
+	PyBUF_INDIRECT = PyBUF_Flag(C.PyBUF_INDIRECT)
+	PyBUF_FORMAT = PyBUF_Flag(C.PyBUF_FORMAT)
+	PyBUF_STRIDED = PyBUF_Flag(C.PyBUF_STRIDED)
+	PyBUF_STRIDED_RO = PyBUF_Flag(C.PyBUF_STRIDED_RO)
+	PyBUF_RECORDS = PyBUF_Flag(C.PyBUF_RECORDS)
+	PyBUF_RECORDS_RO = PyBUF_Flag(C.PyBUF_RECORDS_RO)
+	PyBUF_FULL = PyBUF_Flag(C.PyBUF_FULL)
+	PyBUF_FULL_RO = PyBUF_Flag(C.PyBUF_FULL_RO)
+	PyBUF_CONTIG = PyBUF_Flag(C.PyBUF_CONTIG)
+	PyBUF_CONTIG_RO = PyBUF_Flag(C.PyBUF_CONTIG_RO)
+)
+
+/*
+int PyObject_CheckBuffer(PyObject *obj)
+Return 1 if obj supports the buffer interface otherwise 0.
+*/
+func PyObject_CheckBuffer(self *PyObject) bool {
+	return int2bool(C._gopy_PyObject_CheckBuffer(topy(self)))
+}
+
+/*
+int PyObject_GetBuffer(PyObject *obj, Py_buffer *view, int flags)
+Export obj into a Py_buffer, view. These arguments must never be NULL. The flags argument is a bit field indicating what kind of buffer the caller is prepared to deal with and therefore what kind of buffer the exporter is allowed to return. The buffer interface allows for complicated memory sharing possibilities, but some caller may not be able to handle all the complexity but may want to see if the exporter will let them take a simpler view to its memory.
+
+Some exporters may not be able to share memory in every possible way and may need to raise errors to signal to some consumers that something is just not possible. These errors should be a BufferError unless there is another error that is actually causing the problem. The exporter can use flags information to simplify how much of the Py_buffer structure is filled in with non-default values and/or raise an error if the object can’t support a simpler view of its memory.
+
+0 is returned on success and -1 on error.
+
+The following table gives possible values to the flags arguments.
+
+Flag	Description
+PyBUF_SIMPLE	This is the default flag state. The returned buffer may or may not have writable memory. The format of the data will be assumed to be unsigned bytes. This is a “stand-alone” flag constant. It never needs to be ‘|’d to the others. The exporter will raise an error if it cannot provide such a contiguous buffer of bytes.
+PyBUF_WRITABLE	The returned buffer must be writable. If it is not writable, then raise an error.
+PyBUF_STRIDES	This implies PyBUF_ND. The returned buffer must provide strides information (i.e. the strides cannot be NULL). This would be used when the consumer can handle strided, discontiguous arrays. Handling strides automatically assumes you can handle shape. The exporter can raise an error if a strided representation of the data is not possible (i.e. without the suboffsets).
+PyBUF_ND	The returned buffer must provide shape information. The memory will be assumed C-style contiguous (last dimension varies the fastest). The exporter may raise an error if it cannot provide this kind of contiguous buffer. If this is not given then shape will be NULL.
+PyBUF_C_CONTIGUOUS PyBUF_F_CONTIGUOUS PyBUF_ANY_CONTIGUOUS	These flags indicate that the contiguity returned buffer must be respectively, C-contiguous (last dimension varies the fastest), Fortran contiguous (first dimension varies the fastest) or either one. All of these flags imply PyBUF_STRIDES and guarantee that the strides buffer info structure will be filled in correctly.
+PyBUF_INDIRECT	This flag indicates the returned buffer must have suboffsets information (which can be NULL if no suboffsets are needed). This can be used when the consumer can handle indirect array referencing implied by these suboffsets. This implies PyBUF_STRIDES.
+PyBUF_FORMAT	The returned buffer must have true format information if this flag is provided. This would be used when the consumer is going to be checking for what ‘kind’ of data is actually stored. An exporter should always be able to provide this information if requested. If format is not explicitly requested then the format must be returned as NULL (which means 'B', or unsigned bytes)
+PyBUF_STRIDED	This is equivalent to (PyBUF_STRIDES | PyBUF_WRITABLE).
+PyBUF_STRIDED_RO	This is equivalent to (PyBUF_STRIDES).
+PyBUF_RECORDS	This is equivalent to (PyBUF_STRIDES | PyBUF_FORMAT | PyBUF_WRITABLE).
+PyBUF_RECORDS_RO	This is equivalent to (PyBUF_STRIDES | PyBUF_FORMAT).
+PyBUF_FULL	This is equivalent to (PyBUF_INDIRECT | PyBUF_FORMAT | PyBUF_WRITABLE).
+PyBUF_FULL_RO	This is equivalent to (PyBUF_INDIRECT | PyBUF_FORMAT).
+PyBUF_CONTIG	This is equivalent to (PyBUF_ND | PyBUF_WRITABLE).
+PyBUF_CONTIG_RO	This is equivalent to (PyBUF_ND).
+*/
+func PyObject_GetBuffer(self *PyObject, flags PyBUF_Flag) (buf *Py_buffer, err os.Error) {
+	buf.ptr = &C.Py_buffer{}
+	err = int2err(C.PyObject_GetBuffer(topy(self), buf.ptr, C.int(flags)))
+	return
+}
+/*
+void PyBuffer_Release(Py_buffer *view)
+Release the buffer view. This should be called when the buffer is no longer being used as it may free memory from it.
+*/
+func PyBuffer_Release(self *Py_buffer) {
+	C.PyBuffer_Release(self.ptr)
+}
+
+/*
+Py_ssize_t PyBuffer_SizeFromFormat(const char *)
+Return the implied ~Py_buffer.itemsize from the struct-stype ~Py_buffer.format.
+*/
+func PyBuffer_SizeFromFormat(self *Py_buffer) int {
+	//FIXME
+	panic("not implemented")
+}
+
+/*
+int PyObject_CopyToObject(PyObject *obj, void *buf, Py_ssize_t len, char fortran)
+Copy len bytes of data pointed to by the contiguous chunk of memory pointed to by buf into the buffer exported by obj. The buffer must of course be writable. Return 0 on success and return -1 and raise an error on failure. If the object does not have a writable buffer, then an error is raised. If fortran is 'F', then if the object is multi-dimensional, then the data will be copied into the array in Fortran-style (first dimension varies the fastest). If fortran is 'C', then the data will be copied into the array in C-style (last dimension varies the fastest). If fortran is 'A', then it does not matter and the copy will be made in whatever way is more efficient.
+*/
+func PyObject_CopyToObject(self *PyObject, buf []byte, fortran string) os.Error {
+	/*
+	c_buf := (*C.char)(unsafe.Pointer(&buf[0]))
+	c_for := C.char(fortran[0])
+
+	py_self := self.ptr
+	//defer C.free(unsafe.Pointer(c_for))
+	err := C.PyObject_CopyToObject(py_self, c_buf, C.Py_ssize_t(len(buf)), c_for)
+	 */
+	//FIXME
+	panic("not implemented")
+}
+
+/*
+int PyBuffer_IsContiguous(Py_buffer *view, char fortran)
+Return 1 if the memory defined by the view is C-style (fortran is 'C') or Fortran-style (fortran is 'F') contiguous or either one (fortran is 'A'). Return 0 otherwise.
+*/
+func PyBuffer_IsContiguous(self *Py_buffer, fortran string) bool {
+	c_fortran := C.char(fortran[0])
+	return int2bool(C.PyBuffer_IsContiguous(self.ptr, c_fortran))
+}
+
+/*
+void PyBuffer_FillContiguousStrides(int ndim, Py_ssize_t *shape, Py_ssize_t *strides, Py_ssize_t itemsize, char fortran)
+Fill the strides array with byte-strides of a contiguous (C-style if fortran is 'C' or Fortran-style if fortran is 'F' array of the given shape with the given number of bytes per element.
+*/
+func PyBuffer_FillContiguousStrides(ndim int, shape, strides []int, itemsize int, fortran string) {
+	//FIXME
+	panic("not implemented")
+}
+
+/*
+int PyBuffer_FillInfo(Py_buffer *view, PyObject *obj, void *buf, Py_ssize_t len, int readonly, int infoflags)
+Fill in a buffer-info structure, view, correctly for an exporter that can only share a contiguous chunk of memory of “unsigned bytes” of the given length. Return 0 on success and -1 (with raising an error) on error.
+*/
+func PyBuffer_FillInfo(self *PyObject, buf []byte, readonly bool, infoflags int) (buffer *Py_buffer, err os.Error) {
+	//FIXME
+	panic("not implemented")
+}
+
 
 // EOF
