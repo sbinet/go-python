1ae87b22e9ddb9001c6226c709116fff12d5ae80
Sebastien Binet <binet@farnsworth>
2011-01-03 18:37:03 +0100

    add PyModule and PyOS

--- pkg/otherobjects.go
+++ pkg/otherobjects.go
@@ -10,7 +10,8 @@ int _gopy_PyModule_CheckExact(PyObject *p) { return PyModule_CheckExact(p); }
 
 */
 import "C"
-//import "unsafe"
+import "unsafe"
+import "os"
 
 ///// module /////
 
@@ -38,40 +39,104 @@ func PyModule_CheckExact(self *PyObject) bool {
 PyObject* PyModule_New(const char *name)
 Return value: New reference.
 Return a new module object with the __name__ attribute set to name. Only the module’s __doc__ and __name__ attributes are filled in; the caller is responsible for providing a __file__ attribute.
+*/
+func PyModule_New(name string) *PyObject {
+	c_name := C.CString(name)
+	defer C.free(unsafe.Pointer(c_name))
 
+	return togo(C.PyModule_New(c_name))
+}
+
+/*
 PyObject* PyModule_GetDict(PyObject *module)
 Return value: Borrowed reference.
 Return the dictionary object that implements module‘s namespace; this object is the same as the __dict__ attribute of the module object. This function never fails. It is recommended extensions use other PyModule_*() and PyObject_*() functions rather than directly manipulate a module’s __dict__.
+*/
+func PyModule_GetDict(self *PyObject) *PyObject {
+	return togo(C.PyModule_GetDict(topy(self)))
+}
 
+/*
 char* PyModule_GetName(PyObject *module)
 Return module‘s __name__ value. If the module does not provide one, or if it is not a string, SystemError is raised and NULL is returned.
+*/
+func PyModule_GetName(self *PyObject) string {
+	c_name := C.PyModule_GetName(topy(self))
+	// we do not own c_name...
+	return C.GoString(c_name)
+}
 
+/*
 char* PyModule_GetFilename(PyObject *module)
 Return the name of the file from which module was loaded using module‘s __file__ attribute. If this is not defined, or if it is not a string, raise SystemError and return NULL.
+*/
+func PyModule_GetFilename(self *PyObject) string {
+	c_name := C.PyModule_GetFilename(topy(self))
+	// we do not own c_name...
+	return C.GoString(c_name)
+}
 
+/*
 int PyModule_AddObject(PyObject *module, const char *name, PyObject *value)
 Add an object to module as name. This is a convenience function which can be used from the module’s initialization function. This steals a reference to value. Return -1 on error, 0 on success.
 
 New in version 2.0.
+*/
+func PyModule_AddObject(self *PyObject, name string, value *PyObject) os.Error {
+	c_name := C.CString(name)
+	defer C.free(unsafe.Pointer(c_name))
 
+	return int2err(C.PyModule_AddObject(topy(self), c_name, topy(value)))
+}
+
+/*
 int PyModule_AddIntConstant(PyObject *module, const char *name, long value)
 Add an integer constant to module as name. This convenience function can be used from the module’s initialization function. Return -1 on error, 0 on success.
 
 New in version 2.0.
+*/
+func PyModule_AddIntConstant(self *PyObject, name string, value int) os.Error {
+	c_name := C.CString(name)
+	defer C.free(unsafe.Pointer(c_name))
 
+	return int2err(C.PyModule_AddIntConstant(topy(self), c_name, C.long(value)))
+}
+
+/*
 int PyModule_AddStringConstant(PyObject *module, const char *name, const char *value)
 Add a string constant to module as name. This convenience function can be used from the module’s initialization function. The string value must be null-terminated. Return -1 on error, 0 on success.
 
 New in version 2.0.
+*/
+func PyModule_AddStringConstant(self *PyObject, name, value string) os.Error {
+	c_name := C.CString(name)
+	defer C.free(unsafe.Pointer(c_name))
 
+	c_value := C.CString(value)
+	defer C.free(unsafe.Pointer(c_value))
+
+	return int2err(C.PyModule_AddStringConstant(topy(self), c_name, c_value))
+}
+
+/*
 int PyModule_AddIntMacro(PyObject *module, macro)
 Add an int constant to module. The name and the value are taken from macro. For example PyModule_AddConstant(module, AF_INET) adds the int constant AF_INET with the value of AF_INET to module. Return -1 on error, 0 on success.
 
 New in version 2.6.
+*/
+func PyModule_AddIntMacro(self *PyObject, macro interface{}) os.Error {
+	//FIXME ?
+	panic("not implemented")
+}
 
+/*
 int PyModule_AddStringMacro(PyObject *module, macro)
 Add a string constant to module.
 New in version 2.6.
 */
+func PyModule_AddStringMacro(self *PyObject, macro interface{}) os.Error {
+	//FIXME ?
+	panic("not implemented")
+}
 
 // EOF
