1ceb7f8c8fd67a6de082cb3409919ed6ff34b132
Sebastien Binet <binet@farnsworth>
2011-01-04 10:40:44 +0100

    add PySlice

--- pkg/otherobjects.go
+++ pkg/otherobjects.go
@@ -22,6 +22,9 @@ int _gopy_PyModule_CheckExact(PyObject *p) { return PyModule_CheckExact(p); }
 
  PyObject* _gopy_PyMethod_GET_SELF(PyObject *meth) { return PyMethod_GET_SELF(meth); }
 
+
+ int _gopy_PySlice_Check(PyObject *ob) { return PySlice_Check(ob); }
+
 */
 import "C"
 import "unsafe"
@@ -368,4 +371,82 @@ New in version 2.6.
 func PyMethod_ClearFreeList() int {
 	return int(C.PyMethod_ClearFreeList())
 }
+
+///// slice /////
+
+type PySliceObject struct {
+	ptr *C.PySliceObject
+}
+
+/*
+int PySlice_Check(PyObject *ob)
+Return true if ob is a slice object; ob must not be NULL.
+*/
+func PySlice_Check(ob *PyObject) bool {
+	return int2bool(C._gopy_PySlice_Check(topy(ob)))
+}
+
+/*
+PyObject* PySlice_New(PyObject *start, PyObject *stop, PyObject *step)
+Return value: New reference.
+Return a new slice object with the given values. The start, stop, and step parameters are used as the values of the slice object attributes of the same names. Any of the values may be NULL, in which case the None will be used for the corresponding attribute. Return NULL if the new object could not be allocated.
+*/
+func PySlice_New(start, stop, step *PyObject) *PyObject {
+	return togo(C.PySlice_New(topy(start), topy(stop), topy(step)))
+}
+
+/*
+int PySlice_GetIndices(PySliceObject *slice, Py_ssize_t length, Py_ssize_t *start, Py_ssize_t *stop, Py_ssize_t *step)
+Retrieve the start, stop and step indices from the slice object slice, assuming a sequence of length length. Treats indices greater than length as errors.
+
+Returns 0 on success and -1 on error with no exception set (unless one of the indices was not None and failed to be converted to an integer, in which case -1 is returned with an exception set).
+
+You probably do not want to use this function. If you want to use slice objects in versions of Python prior to 2.3, you would probably do well to incorporate the source of PySlice_GetIndicesEx(), suitably renamed, in the source of your extension.
+
+Changed in version 2.5: This function used an int type for length and an int * type for start, stop, and step. This might require changes in your code for properly supporting 64-bit systems.
+*/
+func PySlice_GetIndices(slice *PySliceObject, length int) (start, stop, step int, err os.Error) {
+	c_start := C.Py_ssize_t(0)
+	c_stop  := C.Py_ssize_t(0)
+	c_step  := C.Py_ssize_t(0)
+
+	err = int2err(C.PySlice_GetIndices(slice.ptr, C.Py_ssize_t(length),
+		&c_start, &c_stop, &c_step))
+
+	start = int(c_start)
+	stop  = int(c_stop)
+	step  = int(c_step)
+
+	return
+}
+
+/*
+int PySlice_GetIndicesEx(PySliceObject *slice, Py_ssize_t length, Py_ssize_t *start, Py_ssize_t *stop, Py_ssize_t *step, Py_ssize_t *slicelength)
+Usable replacement for PySlice_GetIndices(). Retrieve the start, stop, and step indices from the slice object slice assuming a sequence of length length, and store the length of the slice in slicelength. Out of bounds indices are clipped in a manner consistent with the handling of normal slices.
+
+Returns 0 on success and -1 on error with exception set.
+
+New in version 2.3.
+
+Changed in version 2.5: This function used an int type for length and an int * type for start, stop, step, and slicelength. This might require changes in your code for properly supporting 64-bit systems.
+*/
+func PySlice_GetIndicesEx(slice *PySliceObject, length int) (start, stop, step, slicelength int, err os.Error) {
+
+	c_start := C.Py_ssize_t(0)
+	c_stop  := C.Py_ssize_t(0)
+	c_step  := C.Py_ssize_t(0)
+	c_slice := C.Py_ssize_t(0)
+
+	err = int2err(C.PySlice_GetIndicesEx(slice.ptr, C.Py_ssize_t(length),
+		&c_start, &c_stop, &c_step, &c_slice))
+
+	start = int(c_start)
+	stop  = int(c_stop)
+	step  = int(c_step)
+	slicelength  = int(c_slice)
+
+	return
+}
+
+
 // EOF
