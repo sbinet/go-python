1967380bca719793ce82f6ba0c22a24d7b750718
Sebastien Binet <binet@cern.ch>
2015-01-27 18:51:34 +0100

    heap: fixup Py_InitModule signature. add Py_InitModule3

--- heap.go
+++ heap.go
@@ -2,9 +2,22 @@ package python
 
 /*
 #include <Python.h>
+#include <stdlib.h>
 
 static PyObject* _gopy_InitModule(const char* nm, PyMethodDef* methods) {
 	return Py_InitModule(nm, methods);
+}
+
+static PyObject* _gopy_InitModule3(const char* nm, PyMethodDef* methods, const char *doc) {
+	return Py_InitModule3(nm, methods, doc);
+}
+
+static PyMethodDef* _gopy_malloc_PyMethodDefArray(size_t n) {
+    return (PyMethodDef*)malloc(n * sizeof(PyMethodDef));
+}
+
+static _gopy_set_PyMethodDef(PyMethodDef *array, int i, PyMethodDef *o) {
+    array[i] = *o;
 }
 */
 import "C"
@@ -13,10 +26,56 @@ import (
 	"unsafe"
 )
 
-func Py_InitModule(name string, methods ...PyMethodDef) (*PyObject, error) {
+var (
+	gModules = make(map[string]unsafe.Pointer) // register of [modules - method-definitions]
+)
+
+// cpyMethodDefs creates a C-array of C.PyMethodDef, with a {0} sentinel
+// FIXME(sbinet) convert 'methods' to a *C.PyMethodDef
+func cpyMethodDefs(name string, methods []PyMethodDef) *C.PyMethodDef {
+	if len(methods) <= 0 {
+		return nil
+	}
+
+	n := C.size_t(len(methods) + 1)
+	cmeths := C._gopy_malloc_PyMethodDefArray(n)
+	for i, meth := range methods {
+		cmeth := C.PyMethodDef{
+			ml_name:  C.CString(meth.Name),
+			ml_meth:  (C.PyCFunction)(unsafe.Pointer(&meth.Meth)),
+			ml_flags: C.int(meth.Flags),
+			ml_doc:   C.CString(meth.Doc),
+		}
+		C._gopy_set_PyMethodDef(cmeths, C.int(i), &cmeth)
+	}
+
+	gModules[name] = unsafe.Pointer(cmeths)
+	return cmeths
+}
+
+func Py_InitModule(name string, methods []PyMethodDef) (*PyObject, error) {
 	c_mname := C.CString(name)
 	defer C.free(unsafe.Pointer(c_mname))
-	obj := togo(C._gopy_InitModule(c_mname, nil))
+
+	cmeths := cpyMethodDefs(name, methods)
+
+	obj := togo(C._gopy_InitModule(c_mname, cmeths))
+	if obj == nil {
+		return nil, errors.New("python: internal error; module creation failed.")
+	}
+	return obj, nil
+}
+
+func Py_InitModule3(name string, methods []PyMethodDef, doc string) (*PyObject, error) {
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+
+	cdoc := C.CString(doc)
+	defer C.free(unsafe.Pointer(cdoc))
+
+	cmeths := cpyMethodDefs(name, methods)
+
+	obj := togo(C._gopy_InitModule3(cname, cmeths, cdoc))
 	if obj == nil {
 		return nil, errors.New("python: internal error; module creation failed.")
 	}
