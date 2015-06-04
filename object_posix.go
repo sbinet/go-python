c9a2cf7e017fc7552843be879ad1dc63bdd574a7
Sebastien Binet <binet@cern.ch>
2015-06-04 21:00:23 +0200

    object: use fopen on windows
    on Windows, fdopen is broken.
    use fopen instead.
    
    Fixes #21.

---
+++ object_posix.go
@@ -0,0 +1,21 @@
+package python
+
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdio.h>
+import "C"
+
+import (
+	"os"
+	"unsafe"
+)
+
+// file2py opens a stdC file from a Go os.File.  Note the returned file has
+// been newly opened: the caller must close it with C.fclose(retval).
+func file2py(f *os.File, mode string) *C.FILE {
+	cmode := C.CString(mode)
+	defer C.free(unsafe.Pointer(cmode))
+	fd := f.Fd()
+	file := C.fdopen(C.int(fd), cmode)
+	return file
+}
---
+++ object_windows.go
@@ -0,0 +1,22 @@
+package python
+
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdio.h>
+import "C"
+
+import (
+	"os"
+	"unsafe"
+)
+
+// file2py opens a stdC file from a Go os.File.  Note the returned file has
+// been newly opened: the caller must close it with C.fclose(retval).
+func file2py(f *os.File, mode string) *C.FILE {
+	cmode := C.CString(mode)
+	defer C.free(unsafe.Pointer(cmode))
+	name := C.CString(file.Name())
+	defer C.free(unsafe.Pointer(name))
+	file := C.fopen(name, cmode)
+	return file
+}
