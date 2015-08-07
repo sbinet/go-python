0f12455d6adba6bdf7790e6bd86950611733e212
Thorsten Riess <riess@w3-data.de>
2015-08-07 08:21:00 +0200

    Allow access to python bytearrays as []byte

--- sequence.go
+++ sequence.go
@@ -89,6 +89,19 @@ func PyByteArray_AsString(self *PyObject) string {
 	return C.GoString(c_str)
 }
 
+// Return the contents of bytearray as []bytes
+func PyByteArray_AsBytes(self *PyObject) []byte {
+	length := C._gopy_PyByteArray_GET_SIZE(topy(self))
+	c_str := C.PyByteArray_AsString(topy(self))
+	return C.GoBytes(unsafe.Pointer(c_str),C.int(length))
+}
+
+// Return the contents of bytearray as []bytes, size length
+func PyByteArray_AsBytesN(self *PyObject, length int) []byte {
+	c_str := C.PyByteArray_AsString(topy(self))
+	return C.GoBytes(unsafe.Pointer(c_str),C.int(length))
+}
+
 // int PyByteArray_Resize(PyObject *bytearray, Py_ssize_t len)
 // Resize the internal buffer of bytearray to len.
 func PyByteArray_Resize(self *PyObject, sz int) error {
