f5b703a7461fb85738cf010aa9dd694065b065eb
Sebastien Binet <binet@cern.ch>
2016-01-04 13:51:58 +0100

    tests: wire up tests with go-test

--- python_test.go
+++ python_test.go
@@ -1,7 +1,12 @@
 package python
 
 import (
+	"bytes"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"reflect"
 	"testing"
 )
 
@@ -11,6 +16,96 @@ func TestGoPython(t *testing.T) {
 	if err != nil {
 		t.Fatalf("go-python failed: %v", err)
 	}
+}
+
+type pkg struct {
+	path string
+	want []byte
+}
+
+func testPkg(t *testing.T, table pkg) {
+	workdir, err := ioutil.TempDir("", "go-python-")
+	if err != nil {
+		t.Fatalf("[%s]: could not create workdir: %v\n", table.path, err)
+	}
+	err = os.MkdirAll(workdir, 0644)
+	if err != nil {
+		t.Fatalf("[%s]: could not create workdir: %v\n", table.path, err)
+	}
+	defer os.RemoveAll(workdir)
+
+	pypath := "." + string(os.PathListSeparator) + os.Getenv("PYTHONPATH")
+	os.Setenv("PYTHONPATH", pypath)
+
+	buf := new(bytes.Buffer)
+	cmd := exec.Command("go", "run", "main.go")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = buf
+	cmd.Stderr = buf
+	cmd.Dir = table.path
+	err = cmd.Run()
+	if err != nil {
+		t.Fatalf(
+			"[%s]: error running go-python test: %v\n%v\n",
+			table.path,
+			err,
+			string(buf.Bytes()),
+		)
+	}
+
+	if !reflect.DeepEqual(string(buf.Bytes()), string(table.want)) {
+		diffTxt := ""
+		diffBin, diffErr := exec.LookPath("diff")
+		if diffErr == nil {
+			wantFile, wantErr := os.Create(filepath.Join(workdir, "want.txt"))
+			if wantErr == nil {
+				wantFile.Write(table.want)
+				wantFile.Close()
+			}
+			gotFile, gotErr := os.Create(filepath.Join(workdir, "got.txt"))
+			if gotErr == nil {
+				gotFile.Write(buf.Bytes())
+				gotFile.Close()
+			}
+			if gotErr == nil && wantErr == nil {
+				cmd = exec.Command(diffBin, "-urN",
+					wantFile.Name(),
+					gotFile.Name(),
+				)
+				diff, _ := cmd.CombinedOutput()
+				diffTxt = string(diff) + "\n"
+			}
+		}
+
+		t.Fatalf("[%s]: error running go-python test:\nwant:\n%s\n\ngot:\n%s\n%s",
+			table.path,
+			string(table.want), string(buf.Bytes()),
+			diffTxt,
+		)
+	}
+
+}
+
+func TestKwArgs(t *testing.T) {
+	t.Parallel()
+	testPkg(t, pkg{
+		path: "tests/kw-args",
+		want: []byte(`importing kwargs...
+args=() kwds={}
+args=() kwds={'a': 3}
+`),
+	})
+}
+
+func TestCPickle(t *testing.T) {
+	t.Parallel()
+	testPkg(t, pkg{
+		path: "tests/cpickle",
+		want: []byte(`hello [ foo ]
+cPickle.dumps(foo) = "S'foo'\np1\n."
+cPickle.loads("S'foo'\np1\n.") = "foo"
+`),
+	})
 }
 
 // EOF
