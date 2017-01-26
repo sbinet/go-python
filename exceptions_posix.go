a2acb64fbdf8703949837e5ebf50c71319fe03a4
Massimiliano Pippi <massi@datadoghq.com>
2017-01-26 20:10:15 +0100

    restore build on posix

--- exceptions_posix.go
+++ exceptions_posix.go
@@ -2,6 +2,9 @@
 
 package python
 
+// #include "go-python.h"
+import "C"
+
 // int PySignal_SetWakeupFd(int fd)
 // This utility function specifies a file descriptor to which a '\0' byte will be written whenever a signal is received. It returns the previous such file descriptor. The value -1 disables the feature; this is the initial state. This is equivalent to signal.set_wakeup_fd() in Python, but without any error checking. fd should be a valid file descriptor. The function should only be called from the main thread.
 func PySignal_SetWakeupFd(fd int) int {
